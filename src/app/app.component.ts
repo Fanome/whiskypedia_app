8c29f797bbe4f842953f2f18ceefa791d076e014
Fanome <fabionogueiram@yahoo.com.br>
2024-02-09 13:09:52 -0300

    Tela criar usuario - Validações - V6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../app/model/usuario.model';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,16 @@ export class AppComponent  implements OnInit {
   public loginok: number = 1;
   public usuarios: Usuario = {};
   public telefoneOK: boolean = true;
+  public emailOK: boolean = true;
+  public senhaOK: boolean = true;
 
   public nomeRequired: number = 0;
   public emailRequired: number = 0;
   public telefoneRequired: number = 0;
   public datanasRequired: number = 0;
   public senhaRequired: number = 0;
+
+  emailControl = new FormControl('', Validators.email);
 
   public appPages = [
     { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
@@ -31,6 +36,7 @@ export class AppComponent  implements OnInit {
   } 
   
   ngOnInit(): void {
+    
   }
 
   fazerLogin(){
@@ -42,24 +48,48 @@ export class AppComponent  implements OnInit {
   }
 
   criarUsuario(){
+    this.usuarios = {};
+    this.nomeRequired = 0; 
+    this.emailRequired = 0;
+    this.telefoneRequired = 0;
+    this.datanasRequired = 0;
+    this.senhaRequired = 0
+
+    this.telefoneOK = true;
+    this.emailOK = true;
+
     this.loginok = 3;
   }
-
 
   criarNovoUsuario(): void {
     console.log(this.usuarios);
 
     this.validarCampos();
     this.validarCelular();
+    this.validarEmail();
+    this.validarSenha();
 
     if(this.nomeRequired == 0 && this.emailRequired == 0 &&  this.telefoneRequired == 0 &&  this.datanasRequired == 0 && this.senhaRequired == 0){
+      var ok: boolean = true;
+
       if(this.telefoneOK == false){
-        this.loginok = 3;
+        ok = false;
       }
-      else{
+
+      if(this.emailOK == false){
+        ok = false;
+      }
+
+      if(this.senhaOK == false){
+        ok = false;
+      }
+
+      if(ok){
         alert("Usuário cadastrado com sucesso!");
         this.usuarios = {};
         this.loginok = 1;
+      }else{
+        this.loginok = 3;
       }
     }  
   }
@@ -67,11 +97,11 @@ export class AppComponent  implements OnInit {
   validarCelular(){
     if(this.usuarios.telefone_usuario == "" ){
       this.usuarios.telefone_usuario = undefined
-      alert("Favor preencher o Celular.");
+      alert("Favor preencher o celular.");
       this.telefoneOK = false;
     }else{
       if(this.usuarios.telefone_usuario != undefined && this.usuarios.telefone_usuario.length < 11 ){
-        alert("Telefone de ter 11 digitos somando com o DDD.");
+        alert("Telefone deve ter 11 digitos somando com o DDD.");
         this.telefoneOK = false;
       }else{
         this.telefoneOK = true;
@@ -79,32 +109,59 @@ export class AppComponent  implements OnInit {
     }
   }
 
+  validarEmail(){
+    var email: string = '';
+    email = this.usuarios.email_usuario == undefined ? "" : this.usuarios.email_usuario;
+
+    if(email != ""){
+      // Expressão regular para validar o formato do email
+      const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      if(!regex.test(email)){
+        alert("E-mail invalido.");
+        this.emailOK = false;
+      }else{
+        this.emailOK = true;
+      }
+    }
+  }
+
+  validarSenha(){
+    if(!(this.usuarios.senha_usuario == undefined || this.usuarios.senha_usuario == "")){
+      if(this.usuarios.senha_usuario != undefined && this.usuarios.senha_usuario.length < 8 ){
+        alert("Senha deve ter pelo menos 8 caracteres.");
+        this.senhaOK = false;
+      }else{
+        this.senhaOK = true;
+      }
+    }
+  }
+
   validarCampos(){
-    if(this.usuarios.nome_usuario == undefined){
+    if(this.usuarios.nome_usuario == undefined || this.usuarios.nome_usuario == ""){
       this.nomeRequired = 1;
     }else{
       this.nomeRequired = 0;
     }
 
-    if(this.usuarios.email_usuario == undefined){
+    if(this.usuarios.email_usuario == undefined || this.usuarios.email_usuario == ""){
       this.emailRequired = 1;
     }else{
       this.emailRequired = 0;
     }
 
-    if(this.usuarios.telefone_usuario == undefined){
+    if(this.usuarios.telefone_usuario == undefined || this.usuarios.telefone_usuario == ""){
       this.telefoneRequired = 1;
     }else{
       this.telefoneRequired = 0;
     }
 
-    if(this.usuarios.data_nascimento_usuario == undefined){
+    if(this.usuarios.data_nascimento_usuario == undefined || this.usuarios.data_nascimento_usuario == ""){
       this.datanasRequired = 1;
     }else{
       this.datanasRequired = 0;
     }
 
-    if(this.usuarios.senha_usuario == undefined){
+    if(this.usuarios.senha_usuario == undefined || this.usuarios.senha_usuario == ""){
       this.senhaRequired = 1;
     }else{
       this.senhaRequired = 0;
